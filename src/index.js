f98f757734aaf7d63081a396dc9f44d2df98dbd0
wangling <2016644182@qq.com>
2022-01-14 12:26:40 +0800

    1.提取css单独文件 2.css压缩 3.less文件 4.css加兼容性前缀 5.css压缩和js压缩

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+
+import './css/index.css'
+import './css/index.less'
+debugger
 const a = () => {
   console.log('哈哈12')
 }
 a()
+console.log('yichle')
+
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,15 +2,19 @@ const path = require('path')
 const webpack = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const  MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
-  devtool: 'source-map',
-  watch:true,
+  devtool: 'eval-source-map',
+  watch:false,
   watchOptions:{ // 监控的选项
     poll:1000, // 每秒 问我 1000次
     aggregateTimeout:500, // 防抖 我一直输入代码
     ignored:/node_modules/ // 不需要进行监控哪个文件
   },
-  mode: 'development',  // 模式 默认两种 production development
+  mode: 'production',  // 模式 默认两种 production development
   entry: './src/index.js',
   output: {
     filename: 'js/[name].[hash].js', //这个主要作用是将打包后的js已hash值的编码方式来生成出来
@@ -24,6 +28,26 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // {
+          //   loader: 'style-loader',
+          //   options:{
+          //     insertAt:'top'
+          //   }
+          // },
+          {
+            loader: 'css-loader'
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [/*'style-loader'*/MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader']
+      },
       {
         test: /\.js$/,
         use: {
@@ -41,6 +65,31 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      fileName: 'main.css'
     })
-  ]
+  ],
+  // 优化项
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      // 压缩css,注意使用了这个插件，js就不会自动压缩了，需要用
+      new optimizeCssAssetsWebpackPlugin(),
+      // 压缩js
+      new TerserPlugin({
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log'], // 移除console
+          },
+        },
+      })
+    ],
+  },
 }
